ba392b07c0202609098276f6fa82471ca426c18a
robolux <HyperDrive@174.37-191-175.fiber.lynet.no>
2018-10-28 18:37:00 +0100

    transition towards for loop structure complete

--- assignments/assignment2/src/mlp.py
+++ assignments/assignment2/src/mlp.py
@@ -4,6 +4,7 @@
 
 import numpy as nmp
 import math
+import time
 
 class mlp:
 	nmp.seterr(over='ignore')
@@ -53,10 +54,12 @@ class mlp:
 
 	def train(self, inputs, targets, iterations=100):
 
+		# using for loops for nodes
 		def node_mult(var1, var2):
 			mult = []
 			hid_out_u = []
 			hid_out = []
+
 			c_size = nmp.shape(var2)
 			for mm in range(len(var1)):
 				hid_out_u = []
@@ -69,6 +72,36 @@ class mlp:
 						mult.append(mult_trash)
 					hid_out_u.append(sum(mult))
 				hid_out.append(hid_out_u)
+			return hid_out
+
+		# not used - tried to rewrite list operations into numpy operations
+		# but slowed down code, keeping for future reference
+		def node_mult_array(var1, var2):
+
+			# preallocate
+			c_size = nmp.shape(var2)
+			mult_o = nmp.zeros(shape=(1,c_size[0]))
+			mult_hid_out_u = nmp.zeros(shape=(1,c_size[1]))
+			mult_hid_out = nmp.zeros(shape=(1,len(var1)),dtype=object)
+			hid_out = nmp.zeros(shape=(len(var1),c_size[1]))
+			#print(c_size)
+
+			for mm in range(len(var1)):
+				#mult_hid_out_u = mult_hid_out_u*0
+				for rx in range(c_size[1]):
+					#mult_o = mult_o*0
+					for tt in range(c_size[0]):
+						yy = var1[mm][tt]
+						ui = var2[tt][rx]
+						mult_trash = yy * ui
+						mult_o[0,tt] = mult_trash
+					mult_hid_out_u[0,rx] = nmp.sum(mult_o)
+				mult_hid_out[0,mm] = mult_hid_out_u[0]
+
+			for hu in range(len(var1)):
+				for hn in range(c_size[1]):
+					hid_out[hu][hn] = mult_hid_out[0][hu][hn]
+
 			return hid_out
 
 		for i in range(iterations):
@@ -86,14 +119,11 @@ class mlp:
 			    rearrange_ol.append(l2)
 			rearrange_ol = nmp.asarray(rearrange_ol)
 
-
 			hid_out = node_mult(outputdelta, rearrange_ol)
 
 			hid_out = nmp.asarray(hid_out)
 
 			hiddendelta = hid_out*(hiddenL*(1-hiddenL))
-
-			# hiddendelta = outputdelta.dot(self.output_l_weights.T)*(hiddenL*(1-hiddenL)) # sigmod derivative function
 
 			dim_olw = nmp.shape(hiddenL)
 			rearrange_olw = []
@@ -125,16 +155,39 @@ class mlp:
 			mult_hlw = nmp.asarray(mult_hlw)
 			self.hidden_l_weights -= mult_hlw
 
-			# update weights
-			# self.output_l_weights -= self.eta*hiddenL.T.dot(outputdelta)
-			# self.hidden_l_weights -= self.eta*inputs.T.dot(hiddendelta)
 
 	def forward(self, inputs):
 		#run forward
 
-		hiddenL = 1/(1+nmp.exp(-self.beta*(nmp.dot(inputs, self.hidden_l_weights)))) #activation with sigmoid function
+		def node_mult(var1, var2):
+			mult = []
+			hid_out_u = []
+			hid_out = []
+
+
+			c_size = nmp.shape(var2)
+			for mm in range(len(var1)):
+				hid_out_u = []
+				for rx in range(c_size[1]):
+					mult = []
+					for tt in range(c_size[0]):
+						yy = var1[mm][tt]
+						ui = var2[tt][rx]
+						mult_trash = yy * ui
+						mult.append(mult_trash)
+					hid_out_u.append(sum(mult))
+				hid_out.append(hid_out_u)
+			return hid_out
+
+		mult1 = node_mult(inputs, self.hidden_l_weights)
+		mult1 = nmp.asarray(mult1)
+		hiddenL = 1/(1+nmp.exp(-self.beta*(mult1)))
+
 		hiddenL[:, 0] = self.bias
-		outputL = 1/(1+nmp.exp(-self.beta*(nmp.dot(hiddenL, self.output_l_weights)))) #activation with sigmoid function
+
+		mult2 = node_mult(hiddenL, self.output_l_weights)
+		mult2 = nmp.asarray(mult2)
+		outputL = 1/(1+nmp.exp(-self.beta*(mult2)))
 
 		return hiddenL, outputL
 
